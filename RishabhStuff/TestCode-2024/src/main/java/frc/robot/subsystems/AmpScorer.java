7298bf7975f69820bf29a9ceb322430f86e71742
RishabhK12 <rishabhkapoor@live.com>
2024-01-14 11:39:39 -0700

    Added amp scorer protoype test code

---
+++ RishabhStuff/TestCode-2024/src/main/java/frc/robot/subsystems/AmpScorer.java
@@ -0,0 +1,25 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.subsystems;
+
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkLowLevel.MotorType;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class AmpScorer extends SubsystemBase {
+  CANSparkMax sparkMax = new CANSparkMax(1, MotorType.kBrushless);
+  /** Creates a new AmpScorer. */
+  public AmpScorer() {}
+
+  public void intakeOrOutakeRing(double speed){
+    sparkMax.set(speed);
+  }
+
+  @Override
+  public void periodic() {
+    // This method will be called once per scheduler run
+  }
+}
