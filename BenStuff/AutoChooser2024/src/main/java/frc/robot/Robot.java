cff60ea7d943ab6dd9312dc4feeb5d77e0709f0f
Ben-Mick <benjaminmick@gmail.com>
2024-02-02 20:24:30 -0700

    OI.java complete with working driver controller operator controller, and autoshooser, each printing values to smartdashboard

--- BenStuff/AutoChooser2024/src/main/java/frc/robot/Robot.java
+++ BenStuff/AutoChooser2024/src/main/java/frc/robot/Robot.java
@@ -1,103 +1,103 @@
-// Copyright (c) FIRST and other WPILib contributors.
-// Open Source Software; you can modify and/or share it under the terms of
-// the WPILib BSD license file in the root directory of this project.
-
 package frc.robot;
 
 import edu.wpi.first.wpilibj.TimedRobot;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
 
-/**
- * The VM is configured to automatically run this class, and to call the functions corresponding to
- * each mode, as described in the TimedRobot documentation. If you change the name of this class or
- * the package after creating this project, you must also update the build.gradle file in the
- * project.
- */
 public class Robot extends TimedRobot {
   private Command m_autonomousCommand;
 
   private RobotContainer m_robotContainer;
 
-  /**
-   * This function is run when the robot is first started up and should be used for any
-   * initialization code.
-   */
+
   @Override
   public void robotInit() {
-    // Instantiate our RobotContainer.  This will perform all our button bindings, and put our
-    // autonomous chooser on the dashboard.
     m_robotContainer = new RobotContainer();
   }
 
-  /**
-   * This function is called every 20 ms, no matter the mode. Use this for items like diagnostics
-   * that you want ran during disabled, autonomous, teleoperated and test.
-   *
-   * <p>This runs after the mode specific periodic functions, but before LiveWindow and
-   * SmartDashboard integrated updating.
-   */
   @Override
   public void robotPeriodic() {
-    // Runs the Scheduler.  This is responsible for polling buttons, adding newly-scheduled
-    // commands, running already-scheduled commands, removing finished or interrupted commands,
-    // and running subsystem periodic() methods.  This must be called from the robot's periodic
-    // block in order for anything in the Command-based framework to work.
     CommandScheduler.getInstance().run();
+    SmartDashboard.putBoolean("Switch 1", OI.isSwitch1());
+    SmartDashboard.putBoolean("Switch 2", OI.isSwitch2());
+    SmartDashboard.putBoolean("Switch 3", OI.isSwitch3());
+    SmartDashboard.putBoolean("Dial 1", OI.isDial1());
+    SmartDashboard.putBoolean("Dial 2", OI.isDial2());
+    SmartDashboard.putBoolean("Dial 3", OI.isDial3());
+    SmartDashboard.putBoolean("Dial 4", OI.isDial4());
+    SmartDashboard.putBoolean("Dial 5", OI.isDial5());
+
+    SmartDashboard.putBoolean("dA", OI.getDriverA());
+    SmartDashboard.putBoolean("dB", OI.getDriverB());
+    SmartDashboard.putBoolean("dX", OI.getDriverX());
+    SmartDashboard.putBoolean("dY", OI.getDriverY());
+    SmartDashboard.putBoolean("dRB", OI.getDriverRB());
+    SmartDashboard.putBoolean("dLB", OI.getDriverLB());
+    SmartDashboard.putBoolean("dRT", OI.getDriverRT());
+    SmartDashboard.putBoolean("dLT", OI.getDriverLT());
+    SmartDashboard.putNumber("dLX", OI.getDriverLeftX());
+    SmartDashboard.putNumber("dLY", OI.getDriverLeftY());
+    SmartDashboard.putNumber("dRX", OI.getDriverRightX());
+    SmartDashboard.putNumber("dRY", OI.getDriverRightY());
+    SmartDashboard.putNumber("dPOV", OI.getDriverPOV());
+
+    SmartDashboard.putBoolean("oA", OI.getOperatorA());
+    SmartDashboard.putBoolean("oB", OI.getOperatorB());
+    SmartDashboard.putBoolean("oX", OI.getOperatorX());
+    SmartDashboard.putBoolean("oY", OI.getOperatorY());
+    SmartDashboard.putBoolean("oRB", OI.getOperatorRB());
+    SmartDashboard.putBoolean("oLB", OI.getOperatorLB());
+    SmartDashboard.putBoolean("oRT", OI.getOperatorRT());
+    SmartDashboard.putBoolean("oLT", OI.getOperatorLT());
+    SmartDashboard.putNumber("oLX", OI.getOperatorLeftX());
+    SmartDashboard.putNumber("oLY", OI.getOperatorLeftY());
+    SmartDashboard.putNumber("oRX", OI.getOperatorRightX());
+    SmartDashboard.putNumber("oRY", OI.getOperatorRightY());
+    SmartDashboard.putNumber("oPOV", OI.getOperatorPOV());
+    
+    // System.out.println("S1" + OI.isSwitch1() + "S2" + OI.isSwitch2() + "S3" + OI.isSwitch3() +  "D1" + OI.isDial1() +  "D2" + OI.isDial2() +  "D3" + OI.isDial3() +  "D4" + OI.isDial4() +  "D5" + OI.isDial5());
   }
 
-  /** This function is called once each time the robot enters Disabled mode. */
   @Override
   public void disabledInit() {}
 
   @Override
   public void disabledPeriodic() {}
 
-  /** This autonomous runs the autonomous command selected by your {@link RobotContainer} class. */
   @Override
   public void autonomousInit() {
     m_autonomousCommand = m_robotContainer.getAutonomousCommand();
 
-    // schedule the autonomous command (example)
     if (m_autonomousCommand != null) {
       m_autonomousCommand.schedule();
     }
   }
 
-  /** This function is called periodically during autonomous. */
   @Override
   public void autonomousPeriodic() {}
 
   @Override
   public void teleopInit() {
-    // This makes sure that the autonomous stops running when
-    // teleop starts running. If you want the autonomous to
-    // continue until interrupted by another command, remove
-    // this line or comment it out.
     if (m_autonomousCommand != null) {
       m_autonomousCommand.cancel();
     }
   }
 
-  /** This function is called periodically during operator control. */
   @Override
   public void teleopPeriodic() {}
 
   @Override
   public void testInit() {
-    // Cancels all running commands at the start of test mode.
     CommandScheduler.getInstance().cancelAll();
   }
 
-  /** This function is called periodically during test mode. */
   @Override
   public void testPeriodic() {}
 
-  /** This function is called once when the robot is first started up. */
   @Override
   public void simulationInit() {}
 
-  /** This function is called periodically whilst in simulation. */
   @Override
   public void simulationPeriodic() {}
 }
