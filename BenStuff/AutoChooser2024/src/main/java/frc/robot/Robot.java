28b404dfa9896880ce5446620f5659bb3328eafe
Ben-Mick <benjaminmick@gmail.com>
2024-03-13 10:37:31 -0600

    cleaned up code

--- BenStuff/AutoChooser2024/src/main/java/frc/robot/Robot.java
+++ BenStuff/AutoChooser2024/src/main/java/frc/robot/Robot.java
@@ -10,7 +10,6 @@ public class Robot extends TimedRobot {
 
   private RobotContainer m_robotContainer;
 
-
   @Override
   public void robotInit() {
     m_robotContainer = new RobotContainer();
@@ -19,44 +18,42 @@ public class Robot extends TimedRobot {
   @Override
   public void robotPeriodic() {
     CommandScheduler.getInstance().run();
-    SmartDashboard.putBoolean("Switch 1", OI.isSwitch1());
-    SmartDashboard.putBoolean("Switch 2", OI.isSwitch2());
-    SmartDashboard.putBoolean("Switch 3", OI.isSwitch3());
-    SmartDashboard.putBoolean("Dial 1", OI.isDial1());
-    SmartDashboard.putBoolean("Dial 2", OI.isDial2());
-    SmartDashboard.putBoolean("Dial 3", OI.isDial3());
-    SmartDashboard.putBoolean("Dial 4", OI.isDial4());
-    SmartDashboard.putBoolean("Dial 5", OI.isDial5());
+    // SmartDashboard.putBoolean("Switch 1", OI.isSwitch1());
+    // SmartDashboard.putBoolean("Switch 2", OI.isSwitch2());
+    // SmartDashboard.putBoolean("Switch 3", OI.isSwitch3());
+    // SmartDashboard.putBoolean("Dial 1", OI.isDial1());
+    // SmartDashboard.putBoolean("Dial 2", OI.isDial2());
+    // SmartDashboard.putBoolean("Dial 3", OI.isDial3());
+    // SmartDashboard.putBoolean("Dial 4", OI.isDial4());
+    // SmartDashboard.putBoolean("Dial 5", OI.isDial5());
 
-    SmartDashboard.putBoolean("dA", OI.getDriverA());
-    SmartDashboard.putBoolean("dB", OI.getDriverB());
-    SmartDashboard.putBoolean("dX", OI.getDriverX());
-    SmartDashboard.putBoolean("dY", OI.getDriverY());
-    SmartDashboard.putBoolean("dRB", OI.getDriverRB());
-    SmartDashboard.putBoolean("dLB", OI.getDriverLB());
-    SmartDashboard.putBoolean("dRT", OI.getDriverRT());
-    SmartDashboard.putBoolean("dLT", OI.getDriverLT());
-    SmartDashboard.putNumber("dLX", OI.getDriverLeftX());
-    SmartDashboard.putNumber("dLY", OI.getDriverLeftY());
-    SmartDashboard.putNumber("dRX", OI.getDriverRightX());
-    SmartDashboard.putNumber("dRY", OI.getDriverRightY());
-    SmartDashboard.putNumber("dPOV", OI.getDriverPOV());
+    // SmartDashboard.putBoolean("dA", OI.getDriverA());
+    // SmartDashboard.putBoolean("dB", OI.getDriverB());
+    // SmartDashboard.putBoolean("dX", OI.getDriverX());
+    // SmartDashboard.putBoolean("dY", OI.getDriverY());
+    // SmartDashboard.putBoolean("dRB", OI.getDriverRB());
+    // SmartDashboard.putBoolean("dLB", OI.getDriverLB());
+    // SmartDashboard.putBoolean("dRT", OI.getDriverRT());
+    // SmartDashboard.putBoolean("dLT", OI.getDriverLT());
+    // SmartDashboard.putNumber("dLX", OI.getDriverLeftX());
+    // SmartDashboard.putNumber("dLY", OI.getDriverLeftY());
+    // SmartDashboard.putNumber("dRX", OI.getDriverRightX());
+    // SmartDashboard.putNumber("dRY", OI.getDriverRightY());
+    // SmartDashboard.putNumber("dPOV", OI.getDriverPOV());
 
-    SmartDashboard.putBoolean("oA", OI.getOperatorA());
-    SmartDashboard.putBoolean("oB", OI.getOperatorB());
-    SmartDashboard.putBoolean("oX", OI.getOperatorX());
-    SmartDashboard.putBoolean("oY", OI.getOperatorY());
-    SmartDashboard.putBoolean("oRB", OI.getOperatorRB());
-    SmartDashboard.putBoolean("oLB", OI.getOperatorLB());
-    SmartDashboard.putBoolean("oRT", OI.getOperatorRT());
-    SmartDashboard.putBoolean("oLT", OI.getOperatorLT());
-    SmartDashboard.putNumber("oLX", OI.getOperatorLeftX());
-    SmartDashboard.putNumber("oLY", OI.getOperatorLeftY());
-    SmartDashboard.putNumber("oRX", OI.getOperatorRightX());
-    SmartDashboard.putNumber("oRY", OI.getOperatorRightY());
-    SmartDashboard.putNumber("oPOV", OI.getOperatorPOV());
-    
-    // System.out.println("S1" + OI.isSwitch1() + "S2" + OI.isSwitch2() + "S3" + OI.isSwitch3() +  "D1" + OI.isDial1() +  "D2" + OI.isDial2() +  "D3" + OI.isDial3() +  "D4" + OI.isDial4() +  "D5" + OI.isDial5());
+    // SmartDashboard.putBoolean("oA", OI.getOperatorA());
+    // SmartDashboard.putBoolean("oB", OI.getOperatorB());
+    // SmartDashboard.putBoolean("oX", OI.getOperatorX());
+    // SmartDashboard.putBoolean("oY", OI.getOperatorY());
+    // SmartDashboard.putBoolean("oRB", OI.getOperatorRB());
+    // SmartDashboard.putBoolean("oLB", OI.getOperatorLB());
+    // SmartDashboard.putBoolean("oRT", OI.getOperatorRT());
+    // SmartDashboard.putBoolean("oLT", OI.getOperatorLT());
+    // SmartDashboard.putNumber("oLX", OI.getOperatorLeftX());
+    // SmartDashboard.putNumber("oLY", OI.getOperatorLeftY());
+    // SmartDashboard.putNumber("oRX", OI.getOperatorRightX());
+    // SmartDashboard.putNumber("oRY", OI.getOperatorRightY());
+    // SmartDashboard.putNumber("oPOV", OI.getOperatorPOV());
   }
 
   @Override
